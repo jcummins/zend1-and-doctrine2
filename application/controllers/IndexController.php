6acef52f3cba6dc7a198891cc1e8169f32934e97
Jim Cummins <jim.for.cy@gmail.com>
2013-11-29 13:56:42 -0600

    It's alive. It's alive

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -2,10 +2,12 @@
 
 class IndexController extends Zend_Controller_Action
 {
-
+    protected $em = null;
+    
     public function init()
     {
         /* Initialize action controller here */
+         $this->em = \Zend_Registry::get('doctrine')->getEntityManager();
     }
 
     public function indexAction()
